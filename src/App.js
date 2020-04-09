9772d630c5cd12dacb0cf3cefb05a4aeb2afe460
roxyoanes <roxioanes11@gmail.com>
2020-04-09 10:52:07 +0200

    Display buttons by type

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import { bikeApi } from "./api";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-          hello
-        </p>
-    </div>
-  );
+const App = () => {
+  const [incidents, setIncidents] = React.useState([]);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() =>{
+    bikeApi().then(data => {
+      setIncidents(data.incidents)
+    });
+  }, [])
+
+  const displayCards = () => {
+    bikeApi().then(data => {
+      setIncidents(data.incidents)
+    });
+  }
+
+  if(error){
+  return <p>{error}</p>
+  } else if(incidents){
+
+    const mappedArray = incidents.map(incident => incident.type);
+    const uniqueTypes = [...new Set(mappedArray)];
+    const filteredIncidents = incidents.filter(incident => incident.type);
+
+    return (
+      <div className="App">
+        {uniqueTypes.map((type) => <button onClick={displayCards}>{type}</button> )}
+          <p>
+            hello
+          </p>
+      </div>
+    );
+  } else{
+    return <p>Loading</p>
+  }
 }
 
 export default App;
